f7b0b5e0aa9546c1245438ae7993b18da2bd1a1e
Ashley <panelo.ashley@gmail.com>
2025-06-09 20:15:35 +0800

    Questions now appear inside the canvas

--- ui.py
+++ ui.py
@@ -1,10 +1,13 @@
 from tkinter import *
+from quiz_brain import QuizBrain
 
 THEME_COLOR = "#375362"
 
 class QuizInterface:
 
-    def __init__(self):
+    def __init__(self, quiz_brain: QuizBrain):
+        self.quiz = quiz_brain
+
         self.window = Tk()
         self.window.title("The Best Quiz App in the World")
         self.window.config(padx=20,pady=20, bg=THEME_COLOR)
@@ -15,6 +18,7 @@ class QuizInterface:
         self.canvas = Canvas(width=300, height=250)
         self.question_text = self.canvas.create_text(
             150, 125,
+            width=280,
             fill=THEME_COLOR,
             text=f"Sample Question",
             font=("Ariel", 20, "italic")
@@ -29,4 +33,10 @@ class QuizInterface:
         self.false_button = Button(image=self.false_file, )
         self.false_button.grid(column=1, row=2)
 
+        self.get_next_question()
+
         self.window.mainloop()
+
+    def get_next_question(self):
+        question_text = self.quiz.next_question()
+        self.canvas.itemconfig(self.question_text, text=question_text)
